23ac79a18d64b2fddaf1c8c19a77a7eb5405cdcb
Shruthi Bhatt <shrbhatt@in.ibm.com>
2019-01-04 09:24:46 +0530

    Initial commit with images and data files

---
+++ controllers/productData.js
@@ -0,0 +1,32 @@
+exports.products = [{
+    "name": "Blue Lehenga",
+    "imageUrl": "https://rukminim1.flixcart.com/image/880/1056/jm573ww0/lehenga-choli/4/w/n/free-mdl19-define-jewellery-original-imaf948fythg85yp.jpeg?q=50",
+    "brand": "Saara",
+    "category": "Women",
+    "rating": "4",
+    "price": "370",
+    "description": "This is a blue and pink lehenga with 3/4th sleeve and round neck, suitable for women under 5.5 feet",
+    "size": "M",
+    "color": "Blue"
+  },{
+    "name": "Embroidered red Saree",
+    "imageUrl": "https://rukminim1.flixcart.com/image/880/1056/j3agya80/shirt/c/h/j/s-ropemc-rope-original-imaeufp4w8g7hpfv.jpeg?q=50",
+    "brand": "Biba",
+    "category": "Men",
+    "rating": "4.5",
+    "price": "2370",
+    "description": "This is a silk saree with intricate golden embroidery with a royal look.",
+    "size": "Free",
+    "color": "Red"
+  }, {
+    "name": "Embroidered red Saree",
+    "imageUrl": "https://rukminim1.flixcart.com/image/880/880/jjabekw0/kids-ethnic-set/b/v/t/3-4-years-digibc001-digimart-original-imaffh6mtrzdefhx.jpeg?q=50",
+    "brand": "Biba",
+    "category": "Kids",
+    "rating": "4.5",
+    "price": "2370",
+    "description": "This is a silk saree with intricate golden embroidery with a royal look.",
+    "size": "Free",
+    "color": "Red"
+  }
+];
